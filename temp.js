39dcd226ad9a2e4a50fc31aab4cbad1da1b461c6
nomi7677 <wp061005@gmail.com>
2021-01-06 23:32:09 -0500

    added function to get onclick method work for temp converter

--- temp.js
+++ temp.js
@@ -1,4 +1,32 @@
-  function temperatureConverter(valNum) {
+/*
+1) define a function to get the numeric value from user
+2) use parseFloat function so that it parse an argument and returns a floating point number
+3) define calculation value from F to C using formula F=(9/5)*C + 32
+4) Link the function to respective id in html code
+5) Display output of Celsius on page
+
+
+
+
+
+Connect HTML and JS files
+User clicks button
+Grab the number from the text input
+Calculate the celsius value with the formula c = (f * 9/5) + 32
+Update celsius text
+*/
+
+/*
+function temperatureConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputCelsius").innerHTML=(valNum-32)/1.8;
   }
+  */
+
+
+function tempConvert() {
+  let celsiusCalc = parseFloat(document.getElementById("degrees").value); 
+  document.getElementById("getCelsius").innerHTML = Math.round(
+    celsiusCalc * (5/ 9) - 18 
+  );
+}
